12cc9b558ab8bf6dd9419fadedc0099363fae86d
jasonb-medopad <jason.brewer@medopad.com>
2020-08-04 16:05:07 +0100

    probably could've just used splice

---
+++ typescript-tasks/005-w-mikael/problem.ts
@@ -0,0 +1,17 @@
+// export const adder = (a: number, b: number): number => {
+//     return a + b;
+// }
+
+// take(["apple", "pear", "lemon", "orange"], 2)
+// # ["apple", "pear"]
+
+export const take = (values, numToTake: number) => {
+    let returnArray = []
+    
+    for(let i = 0; i < numToTake; ++i) {
+        const index = i % values.length;
+        returnArray.push(values[index])   
+    }
+
+    return returnArray
+}
