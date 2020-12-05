df1f3a9a8f3a46feb525b288fedb576c991ca573
InBrewJ <jason@urawizard.com>
2020-12-05 12:53:12 +0000

    port complete w/tests passing. Now to figure out the refactor...

--- typescript-tasks/006-merge-sort/MergeSorter.spec.ts
+++ typescript-tasks/006-merge-sort/MergeSorter.spec.ts
@@ -1,21 +1,41 @@
-import { mergeSorter } from "./MergeSorter";
+import { mergeSorter, mergeSorterDesc } from "./MergeSorter";
 
-describe("MergeSorter", () => {
-  it.only("Should return the empty array for an empty array", () => {
+describe("mergeSorter", () => {
+  it("Should return the empty array for an empty array", () => {
     const list = [];
     const sort = [];
     expect(mergeSorter(list)).toEqual(sort);
   });
 
-  it.only("Should sort return the list for length === 1", () => {
+  it("Should sort return the list for length === 1", () => {
     const list = [28];
     const sort = [28];
     expect(mergeSorter(list)).toEqual(sort);
   });
 
-  it("Should sort an unsorted list", () => {
-    const list = [0, 3, 2, 1, 1500, -5];
-    const sort = [-5, 0, 1, 2, 3, 1500];
+  it("Should sort an unsorted list with two elements", () => {
+    const list = [2, 1];
+    const sort = [1, 2];
     expect(mergeSorter(list)).toEqual(sort);
+  });
+
+  it("Should sort an unsorted list with three elements", () => {
+    const list = [45, 2, 16];
+    const sort = [2, 16, 45];
+    expect(mergeSorter(list)).toEqual(sort);
+  });
+
+  it("Should sort an unsorted list with six elements", () => {
+    const list = [10000, 6, 1, 150, 0, 8];
+    const sort = [0, 1, 6, 8, 150, 10000];
+    expect(mergeSorter(list)).toEqual(sort);
+  });
+});
+
+describe("mergeSorterDesc", () => {
+  it("Should sort an unsorted list with six elements", () => {
+    const list = [10000, 6, 1, 150, 0, 8];
+    const sort = [10000, 150, 8, 6, 1, 0];
+    expect(mergeSorterDesc(list)).toEqual(sort);
   });
 });
--- typescript-tasks/006-merge-sort/MergeSorter.ts
+++ typescript-tasks/006-merge-sort/MergeSorter.ts
@@ -1,44 +1,35 @@
 class Sorter {
   private isEmpty = (list: number[]) => list.length === 0;
 
+  private splitAt = (list: number[], pos: number) => {
+    return [list.slice(0, pos), list.slice(pos, list.length)];
+  };
+
   public msort = (less: Function) => (list: number[]) => {
     const merge = (xs: number[], ys: number[]): number[] => {
       if (this.isEmpty(xs) && !this.isEmpty(ys)) {
-        console.log("first pattern");
         return ys;
       }
       if (!this.isEmpty(xs) && this.isEmpty(ys)) {
-        console.log("second pattern");
         return xs;
       }
-      // else
-      console.log(less(xs[0], ys[0]));
 
       if (less(xs[0], ys[0])) {
-        console.log("third pattern");
         const a = merge(xs.slice(1, xs.length), ys);
         a.unshift(xs[0]);
         return a;
       } else {
-        console.log("fourth pattern");
         const b = merge(xs, ys.slice(1, ys.length));
         b.unshift(ys[0]);
         return b;
       }
     };
 
-    const n: number = list.length / 2;
-    console.log("n : ", n);
-    console.log("list : ", list);
-    if (n === 0) return list;
-    else {
-      const [ys, zs] = (() => {
-        const firstHalf = list.slice(0, n);
-        const secondHalf = list.slice(n, list.length);
-        return [firstHalf, secondHalf];
-      })();
-      console.log(ys, zs);
-
+    const n: number = Math.floor(list.length / 2);
+    if (n === 0) {
+      return list;
+    } else {
+      const [ys, zs] = this.splitAt(list, n);
       return merge(this.msort(less)(ys), this.msort(less)(zs));
     }
   };
@@ -47,4 +38,9 @@ class Sorter {
 export const mergeSorter = (list: number[]) => {
   const sorter = new Sorter();
   return sorter.msort((x, y) => x < y)(list);
+};
+
+export const mergeSorterDesc = (list: number[]) => {
+  const sorter = new Sorter();
+  return sorter.msort((x, y) => x > y)(list);
 };
