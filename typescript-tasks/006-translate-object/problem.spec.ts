a882a3dcd1ff21e43bfa6eab87877a8b1b1369b2
inbrewj <jason.brewer@ovoenergy.com>
2021-03-12 13:49:35 +0000

    006 translates the keys of deeply nested objects

--- typescript-tasks/006-translate-object/problem.spec.ts
+++ typescript-tasks/006-translate-object/problem.spec.ts
@@ -21,4 +21,99 @@ describe("translate a typedf object, keys and values alike", () => {
 
     expect(translateObj(input, conf)).toStrictEqual(expected);
   });
+
+  it("translates the keys of a nested object", () => {
+    const input = {
+      un: "1",
+      deux: {
+        un: "1",
+        deux: "2",
+        trois: "3",
+      },
+      trois: "3",
+    };
+    const expected = {
+      one: "1",
+      two: {
+        one: "1",
+        two: "2",
+        three: "3",
+      },
+      three: "3",
+    };
+
+    const conf = new Map<string, string>([
+      ["un", "one"],
+      ["deux", "two"],
+      ["trois", "three"],
+    ]);
+
+    expect(translateObj(input, conf)).toStrictEqual(expected);
+  });
+
+  it("translates the keys of a deeply nested object", () => {
+    const input = {
+      un: "1",
+      deux: {
+        un: "1",
+        deux: "2",
+        trois: {
+          deux: {
+            un: "1",
+            deux: "2",
+            trois: {
+              un: "1",
+            },
+          },
+        },
+      },
+      trois: "3",
+    };
+
+    const expected = {
+      one: "1",
+      two: {
+        one: "1",
+        two: "2",
+        three: {
+          two: {
+            one: "1",
+            two: "2",
+            three: {
+              one: "1",
+            },
+          },
+        },
+      },
+      three: "3",
+    };
+
+    const conf = new Map<string, string>([
+      ["un", "one"],
+      ["deux", "two"],
+      ["trois", "three"],
+    ]);
+
+    expect(translateObj(input, conf)).toStrictEqual(expected);
+  });
+
+  it("should not translates the keys that aren't in the map", () => {
+    const input = {
+      un: "1",
+      deux: "2",
+      trois: "3",
+    };
+    const expected = {
+      one: "1",
+      deux: "2",
+      three: "3",
+    };
+
+    const conf = new Map<string, string>([
+      ["un", "one"],
+      ["trois", "three"],
+    ]);
+
+    expect(translateObj(input, conf)).toStrictEqual(expected);
+  });
 });
--- typescript-tasks/006-translate-object/problem.ts
+++ typescript-tasks/006-translate-object/problem.ts
@@ -1,8 +1,17 @@
 export type TranslationConfig = Map<string, string>;
 
-export const translateObj = (obj, conf: TranslationConfig) => {
+export const translateObj = (
+  obj: Record<string, any>,
+  conf: TranslationConfig
+) => {
   const translated = Object.keys(obj).map((key) => {
-    const val = obj[key];
+    let val = obj[key];
+    const valIsObject = typeof val === "object" && val !== null;
+
+    if (valIsObject) {
+      val = translateObj(val, conf);
+    }
+
     if (conf.has(key)) {
       return [conf.get(key), val];
     } else {
